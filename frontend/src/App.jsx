d3d30fcd12d78ed858bd88bb840ea07bc7a20e36
Aditya Ralhan <152467920+AdityaRalhan@users.noreply.github.com>
2024-10-10 10:49:19 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,42 +1,18 @@
-import React, { useEffect, useState } from 'react';
+import FrontPage from './FrontPage'
+import Model from './Model'
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 
-function App() {
-  const [prediction, setPrediction] = useState('');
 
-  // Function to fetch the latest prediction from Flask
-  const fetchPrediction = async () => {
-    try {
-      const response = await fetch('http://localhost:8080/get_prediction');
-      const data = await response.json();
-      setPrediction(data.prediction);
-    } catch (error) {
-      console.error('Error fetching prediction:', error);
-    }
-  };
-
-  // Call fetchPrediction periodically (e.g., every 1 second)
-  useEffect(() => {
-    const interval = setInterval(fetchPrediction, 1000);  // Fetch every second
-    return () => clearInterval(interval);
-  }, []);
-
+export const App = () => {
   return (
-    <div style={{ textAlign: 'center' }}>
-      <h1>Real-Time Sign Language Detection</h1>
-
-      {/* Video feed from Flask */}
-      <div>
-        <img
-          src="http://localhost:8080/video_feed"
-          alt="Video feed"
-          style={{ width: '80%', border: '1px solid black' }}
-        />
-      </div>
-
-      {/* Display the latest prediction */}
-      <h2>Prediction: {prediction}</h2>
-    </div>
-  );
+    <Router>
+      
+      <Routes>
+          <Route path="/" element={<FrontPage />} />
+          <Route path="/Model" element={<Model />} />
+        </Routes>
+      </Router>
+  )
 }
 
-export default App;
+export default App
